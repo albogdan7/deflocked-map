a7c9d3a011178c0e000ede60c9bd15cd25121bbf
Albert Bogdan <albertbogdan2006@gmail.com>
2026-08-30 19:45:50 -0500

    Switch to OSM tiles with dark CSS filter to remove CARTO watermark

--- frontend/src/components/MapView.tsx
+++ frontend/src/components/MapView.tsx
@@ -640,10 +640,10 @@ export default function MapView({
     <div className="map-container">
       <MapContainer center={[39.5, -98.35]} zoom={5} style={{ height: "100%", width: "100%" }} zoomControl={false}>
         <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          subdomains="abcd"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           maxZoom={19}
+          className="map-tiles-dark"
         />
 
         <MapClickHandler onAdd={onAddWaypoint} />
